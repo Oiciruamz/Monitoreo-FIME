2329ea9d229a137a664a9dad69fddbbed8926a88
EnriqueAS21 <enrique.arreolas@uanl.edu.mx>
2025-04-07 16:11:15 -0600

    ReporteBase de datos

--- js/firebase.js
+++ js/firebase.js
@@ -1,30 +1,37 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
+// Importa las funciones que necesitas de los SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
-   apiKey: "AIzaSyA2ua_OCt1iEi3K3rh0PzqvbX483JmaoTA",
-   authDomain: "monitoreo-5a969.firebaseapp.com",
-   databaseURL: "https://monitoreo-5a969-default-rtdb.firebaseio.com",
-   projectId: "monitoreo-5a969",
-   storageBucket: "monitoreo-5a969.firebasestorage.app",
-   messagingSenderId: "385281802173",
-   appId: "1:385281802173:web:526307890bece5bceedcd4",
-   measurementId: "G-B776FDZQVV"
- };
+// Tu configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA2ua_OCt1iEi3K3rh0PzqvbX483JmaoTA",
+  authDomain: "monitoreo-5a969.firebaseapp.com",
+  databaseURL: "https://monitoreo-5a969-default-rtdb.firebaseio.com",
+  projectId: "monitoreo-5a969",
+  storageBucket: "monitoreo-5a969.firebasestorage.app",
+  messagingSenderId: "385281802173",
+  appId: "1:385281802173:web:526307890bece5bceedcd4",
+  measurementId: "G-B776FDZQVV"
+};
 
- // Initialize Firebase
- firebase.initializeApp(firebaseConfig);
- const database = firebase.database();
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
- function leerDatos() {
-    database.ref('salones/-ONCA6MFmoCbVvs0Jaj9/salones/9101/').once('value').then((snapshot) => {
+function leerDatos() {
+  const dbRef = ref(database, 'salones/-ONCA6MFmoCbVvs0Jaj9/salones/9101/');
+  get(dbRef).then((snapshot) => {
+    if (snapshot.exists()) {
       console.log('Datos:', snapshot.val());
-    });
-  }
+    } else {
+      console.log("No hay datos disponibles.");
+    }
+  }).catch((error) => {
+    console.error("Error al leer los datos:", error);
+  });
+}
 
-  leerDatos()
+export { database };
+leerDatos();
+
