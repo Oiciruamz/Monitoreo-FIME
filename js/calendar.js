bf93a5bde4bd364811703c99c602098913de163a
CrisZR <119096412+CrisZR@users.noreply.github.com>
2025-04-07 17:03:49 -0600

    Update calendar.js
    Ya muestra los eventos de la base de datos, pero aun con errores

--- js/calendar.js
+++ js/calendar.js
@@ -1,13 +1,44 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
+
+
+    fetch('https://monitoreo-5a969-default-rtdb.firebaseio.com/salones.json')
+    .then(response => response.json())
+    .then(data => {
+        const events = [];
+
+        const dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+        const bloques = data['-ONCA6MFmoCbVvs0Jaj9'].bloques;
+        const salones = data['-ONCA6MFmoCbVvs0Jaj9'].salones;
+    
+        for (let salon in salones) {
+            dias.forEach(dia => {
+                if (salones[salon][dia]) {
+                    salones[salon][dia].horarios.forEach(horario => {
+                        const bloquesRango = horario.bloque.split('-');
+                        const inicio = bloques[bloquesRango[0]].inicio;
+                        const fin = bloques[bloquesRango[bloquesRango.length - 1]].fin;
+
+                        events.push({
+                            title: `${horario.materia} - ${horario.profesor}`,
+                            startTime: inicio,
+                            endTime: fin,
+                            daysOfWeek: [dias.indexOf(dia)], 
+                            description: `Grupo: ${horario.grupo} | Salón: ${salon}`
+                        });
+                    });
+                }
+            });
+        }    
+
     var calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridWeek',
         locale: 'es',
         timezone: 'local',
-        hiddenDays: [0, 6], 
+        hiddenDays: [0], 
         slotMinTime: '07:00:00', 
-        slotMaxTime: '21:30:00', 
+        slotMaxTime: '22:00:00', 
         slotDuration: '00:50:00', 
         allDaySlot: false, 
         headerToolbar: false,
@@ -15,13 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
         contentHeight: 'auto',
         slotLabelFormat: { hour: 'numeric', minute: '2-digit', hour12: false }, 
         dayHeaderFormat: { day: 'numeric', weekday: 'long' }, 
-        
-        events: [
-            { title: '9102', start: '2025-03-31T09:00:00', end: '2025-03-31T10:00:00' },
-            { title: '9103', start: '2025-03-31T09:00:00', end: '2025-03-31T10:00:00' },
-            { title: '9104', start: '2025-03-31T09:00:00', end: '2025-03-31T10:00:00' },
-            { title: '9103', start: '2025-04-01T10:20:00', end: '2025-04-01T11:10:00' }
-        ],
+        events: events,
         eventColor: '#FFFFFF',
         eventBackgroundColor: '#96B0FF',
         eventDisplay: 'list-item',
@@ -32,7 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
         eventMaxHeight: 44,
         expandRows: true,
         eventMaxStack: 2,
-
         eventTimeFormat: { 
             hour: '2-digit',
             minute: '2-digit',
@@ -40,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function() {
           }
   
     });
+
 
     calendar.render();
 
@@ -52,5 +77,4 @@ document.addEventListener('DOMContentLoaded', function() {
         calendar.next(); // Navegar a la siguiente semana
     });
 
-});
-  
+})})
